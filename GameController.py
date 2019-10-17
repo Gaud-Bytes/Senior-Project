9e89d70353fe9d483494d1b66ce787692b1c4a5f
Austin <austigau@aol.com>
2019-10-17 15:00:10 -0400

    Refactored some logic and solved event bug. changed some member functions to private since not accessed outside classs

--- GameController.py
+++ GameController.py
@@ -6,30 +6,31 @@ class GameController:
 
     def __init__(self):
         self._root = Tk()
-        #self._root.resizable(False, False)
+        self._root.resizable(False, False)
         self._model = gm()
-        self._view = gv(self._root)
-        self.updateView()
+        self._view = gv(self._root, self._model)
+        self.__bindEvents()
+        self.__updateView()
 
-    
-    def clickSpace(self, event):
+    #PRIVATE functions
+    def __clickSpace(self, event):
         
-        coords = self.pixToGridCoord(event.x, event.y)
+        coords = self.__pixToGridCoord(event.x, event.y)
 
         if(coords[0] == -1 or coords[1] == -1):
             return False
         
         if(self._model.yourTurn() and not self._model.isShipPlacementPhase()):
-            self.attackPhase(coords)
+            self.__attackPhase(coords)
         elif(self._model.isShipPlacementPhase()):
-            self.shipPlacementPhase(coords)
+            self.__shipPlacementPhase(coords)
 
 
         print(coords)
-        self.updateView()
+        self.__updateView()
         return True
 
-    def pixToGridCoord(self, xPix, yPix):
+    def __pixToGridCoord(self, xPix, yPix):
         x1, y1, x2, y2 = (180, 10, 225, 55)
         xCoord, yCoord = (-1, -1)
         yDiff = y2 - y1
@@ -54,18 +55,18 @@ class GameController:
 
         return (xCoord, yCoord)
 
-    def quitgame(self, event):
+    def __quitgame(self, event):
         print("Quitting Game")
         self._root.destroy()
         
 
-    def activeShip(self, index):
+    def __activeShip(self, index):
         aShip = self._model.getPlayerShips()[index]
         self._model.setActiveShip(aShip, index)
 
         print("Active Ship: " + aShip.getName())
-
-    def shipPlacementPhase(self, coords):
+    
+    def __shipPlacementPhase(self, coords):
         selectedSpaces = []
         shipIndex = self._model.getActiveShipIndex()
 
@@ -79,28 +80,33 @@ class GameController:
             self._model.getPlayer().placeShip(self._model.getPlayer(), self._model.getPlayerShips()[shipIndex], selectedSpaces[0], selectedSpaces[1])
             
 
-    def attackPhase(self, coords):
+    def __attackPhase(self, coords):
         self._model.getAIBoard().getSpace(coords[0], coords[1]).toggleSelect()
         
-    def updateView(self):
-        self._view.displayBoards(self._model)
-        self._view.displayButtons(self._model.getPlayerShips())
-        self._view.displayAIShipList(self._model.getAIShips())
+    def __updateView(self):
+        self._view.displayBoards()
+        self._view.displayButtons()
+        self._view.displayAIShipList()
     
-        self._view.getButtons()[0].bind("<Button>", self.quitgame)
-
-        self._view.getButtons()[3].bind("<Button>", lambda event, x=3-3 : self.activeShip(x))
-        self._view.getButtons()[4].bind("<Button>", lambda event, x=4-3 : self.activeShip(x))
-        self._view.getButtons()[5].bind("<Button>", lambda event, x=5-3 : self.activeShip(x))
-        self._view.getButtons()[6].bind("<Button>", lambda event, x=6-3 : self.activeShip(x))
-        self._view.getButtons()[7].bind("<Button>", lambda event, x=7-3 : self.activeShip(x))
-
         if(not self._model.isShipPlacementPhase):
             self._model.setActiveShip(None)
 
-        self._view.getCanvasWidget().bind("<Button-1>", self.clickSpace)
-        self._root.deiconify()
         self._root.mainloop()
+
+    def __bindEvents(self):
+        
+        #button 0-2 quit, confirm, undo.
+        #button 3-8 all ship buttons
+        
+        #bind event to quit button
+        self._view.getButtons()[0].bind("<Button>", self.__quitgame)
+
+        #bind events to each ship button
+        for x in range(3, len(self._view.getButtons())):
+            self._view.getButtons()[x].bind("<Button>", lambda event, y=x-3 : self.__activeShip(y))
+
+        #bind click event
+        self._view.getCanvasWidget().bind("<Button-1>", self.__clickSpace)
 
 gc = GameController()
 
--- GameView.py
+++ GameView.py
@@ -5,7 +5,12 @@ from Board import Board
 class GameView:
     
 
-    def __init__(self, master):
+    def __init__(self, master, model):
+
+        if(not isinstance(model, gm) or not isinstance(master, Tk)):
+            return
+
+        self._model = model
         self._canvas_width, self._canvas_height = (800, 900)
         self._boardXmin, self._boardyMin, self._boardXmax, self._boardYmax =  (0, 0, 0, 0)
         self._master = master
@@ -14,118 +19,138 @@ class GameView:
         self._w.pack()
         self._buttons = []
         self._shipList = []
+        self._boardSpaces = []
 
+        self.__createButtons()
+        self.__createLabels()
+        self.__createBoard()
+
+#PUBLIC FUNCTIONS
     def getCanvasWidget(self):
         return self._w
 
+    def getButtons(self):
+        return self._buttons
 
-    def displayButtons(self, plShips):
+    def displayButtons(self):
+
+        #quitButton
+        self._buttons[0].configure(width =10, bg='#800000', fg="#FFFFFF")
+        
+        #confirmButton
+        self._buttons[1].configure(width=10, bg='#D4AF37')
+
+        #undoButton
+        self._buttons[2].configure(width=10, bg='#E0115F')
+
+        #all the ship buttons
+        for x in range(3, len(self._buttons)):
+            self._buttons[x].configure(width=10, bg='#ADD8E6', fg="#000000")
+                
+
+    def displayAIShipList(self):
+        
+        #Title Label
+        self._shipList[0].configure(width=10, font=("Helvetica", 12))
+
+        #each ships Label
+        for x in range(1, len(self._shipList)):
+            self._shipList[x].configure(width=10, font=("Helvetica bold", 12))
+
+    def displayBoards(self):
+        for i in range(len(self._boardSpaces)):
+            for j in range(len(self._boardSpaces[i])):
+                if self._model.yourTurn() and not self._model.isShipPlacementPhase():
+                    if(self._model.getAIBoard().getSpace(i,j).isAttacked() and self._model.getAIBoard().getSpace(i,j).isOccupied()):
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#B22222", width=1)
+                    elif(self._model.getAIBoard().getSpace(i,j).isAttacked() and not self._model.getAIBoard().getSpace(i,j).isOccupied()):
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#FFFFFF", width=1)
+                    else:
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#228B22", width=1)
+
+                    if(self._model.getAIBoard().getSpace(i,j).isSelected()):
+                        self._w.itemconfig(self._boardSpaces[i][j], outline="#FF6347", width=3)
+                    else:
+                        self._w.itemconfig(self._boardSpaces[i][j], outline="#000000", width=1)
+
+                else:
+                    if(self._model.getPlayerBoard().getSpace(i,j).isAttacked() and self._model.getPlayerBoard().getSpace(i,j).isOccupied()):
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#B22222", width=1)
+                    elif(self._model.getPlayerBoard().getSpace(i,j).isAttacked() and not self._model.getPlayerBoard().getSpace(i,j).isOccupied()):
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#FFFFFF", width=1)
+                    elif(self._model.getPlayerBoard().getSpace(i, j).isOccupied() and not self._model.getPlayerBoard.getSpace(i,j).isAttacked()):
+                        self._w.itemconfig(self._boardSpaces[i][j], fill="#AAA9AD", width=1)
+                    else:
+                        self._w.itemconfig(self._boardSpaces[i][j],fill="#0BB8FF", width=1)
+
+                    if(self._model.getPlayerBoard().getSpace(i,j).isSelected()):
+                        self._w.itemconfig(self._boardSpaces[i][j], outline="#FF6347", width=3)
+                    else:
+                        self._w.itemconfig(self._boardSpaces[i][j], outline="#000000", width=1)
+
+       
+    def __createButtons(self):
+
         buttonX = 150
 
         quitB = Button(self._w, text="Quit", anchor=W)
-        quitB.configure(width =10, bg='#800000', fg="#FFFFFF")
-        quitB_window = self._w.create_window(buttonX, 500, anchor=S, window = quitB)
-    
+        quitB_window = self._w.create_window(buttonX, 500, anchor=S, window=quitB)
 
         confirm = Button(self._w, text="Confrim", anchor=W)
-        confirm.configure(width=10, bg='#D4AF37')
         confirm_window = self._w.create_window(350, 600, anchor=S, window=confirm)
 
         undo = Button(self._w, text="Undo", anchor=W)
-        undo.configure(width=10, bg='#E0115F')
         undo_window = self._w.create_window(450, 600, anchor=S, window=undo)
 
         self._buttons.append(quitB)
         self._buttons.append(confirm)
         self._buttons.append(undo)
 
-        for x in range(len(plShips)):
+        for x in range(len(self._model.getPlayerShips())):
             buttonX += 100
+            shipButton = Button(self._w, text=self._model.getPlayerShips()[x].getName(), anchor=W)
+            shipButton_window = self._w.create_window(buttonX, 500, anchor=S, window=shipButton)
+            self._buttons.append(shipButton)
 
-            if not plShips[x].isSunk():
-                shipButton = Button(self._w, text=plShips[x].getName(), anchor=W)
-                shipButton.configure(width=10, bg='#ADD8E6', fg="#000000")
-                shipButton_window = self._w.create_window(buttonX, 500, anchor=S, window=shipButton)
-                self._buttons.append(shipButton)
-
-    def getButtons(self):
-        return self._buttons
-
-    def displayAIShipList(self, aiShips):
+###PRIVATE functions
+    def __createLabels(self):
         labelX = 150
 
         title = Label(self._w, text="AI Ships:", anchor=W, bg='#AAA9AD' )
-        title.configure(width=10, font=("Helvetica", 12))
         title_window = self._w.create_window(labelX, 550, anchor=S, window=title)
 
         self._shipList.append(title)
 
-        for x in range(len(aiShips)):
+        for x in range(len(self._model.getAIShips())):
             labelX += 100
-            if not aiShips[x].isSunk():
-                shipLabel = Label(self._w, text=aiShips[x].getName(), anchor=W, bg='#AAA9AD' )
-                shipLabel.configure(width=10, font=("Helvetica bold", 12))
-                shipLabel_window = self._w.create_window(labelX, 550, anchor=S, window=shipLabel)
-            
-                self._shipList.append(shipLabel)
+            shipLabel = Label(self._w, text=self._model.getAIShips()[x].getName(), anchor=W, bg='#AAA9AD' )
+            shipLabel.configure(width=10, font=("Helvetica bold", 12))
+            shipLabel_window = self._w.create_window(labelX, 550, anchor=S, window=shipLabel)
+            self._shipList.append(shipLabel)
 
-    def displayBoards(self, model):
-
-        if(not isinstance(model, gm)):
-            return False
-
+    def __createBoard(self):
         x1, y1, x2, y2 = (180, 10, 224, 54)
         yDiff = y2 - y1 + 1
         xDiff = x2 - x1 + 1
 
         self._boardXmin, self._boardYmin = (x1, y1)
-        self._boardXmax, self._boardYmax = (xDiff * model.getPlayerBoard().getRows() + self._boardXmin, yDiff * model.getPlayerBoard().getCols() + self._boardYmin )
+        self._boardXmax, self._boardYmax = (xDiff * self._model.getPlayerBoard().getRows() + self._boardXmin, yDiff * self._model.getPlayerBoard().getCols() + self._boardYmin )
         
+        for x in range(self._model.getPlayerBoard().getRows()):
+            self._boardSpaces.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
         space = NONE
+        for i in range(self._model.getPlayerBoard().getRows()):
+            y1 = 10
+            y2 = 54
+            yDiff = y2 - y1 + 1
+            for j in range(self._model.getPlayerBoard().getCols()):
+                self._boardSpaces[i][j] = self._w.create_rectangle(x1, y1, x2, y2, fill="#0BB8FF", width=1)
+                 
 
-        self._w.delete(ALL)
-
-        if model.yourTurn() and not model.isShipPlacementPhase():
-            for i in range(model.getAIBoard().getRows()):
-                y1 = 10
-                y2 = 54
-                yDiff = y2 - y1 + 1
-                for j in range(model.getAIBoard().getCols()):
-                    if(model.getAIBoard().getSpace(i,j).isAttacked() and model.getAIBoard().getSpace(i,j).isOccupied()):
-                        space = self._w.create_rectangle(x1, y1, x2, y2, fill="#B22222", width=1)
-                    elif(model.getAIBoard().getSpace(i,j).isAttacked() and not model.getAIBoard().getSpace(i,j).isOccupied()):
-                        space =self._w.create_rectangle(x1, y1, x2, y2, fill="#FFFFFF", width=1)
-                    else:
-                        space = self._w.create_rectangle(x1, y1, x2, y2, fill="#228B22", width=1)
-
-                    if(model.getAIBoard().getSpace(i,j).isSelected()):
-                        self._w.itemconfig(space, outline="#FF6347", width=3)
-
-                    y1+=yDiff
-                    y2+=yDiff
-                x1+=xDiff
-                x2+=xDiff
-        else:
-            for i in range(model.getPlayerBoard().getRows()):
-                y1 = 10
-                y2 = 54
-                yDiff = y2 - y1 + 1
-                for j in range(model.getPlayerBoard().getCols()):
-                    if(model.getPlayerBoard().getSpace(i,j).isAttacked() and model.getPlayerBoard().getSpace(i,j).isOccupied()):
-                        space = self._w.create_rectangle(x1, y1, x2, y2, fill="#B22222", width=1)
-                    elif(model.getPlayerBoard().getSpace(i,j).isAttacked() and not model.getPlayerBoard().getSpace(i,j).isOccupied()):
-                       space = self._w.create_rectangle(x1, y1, x2, y2, fill="#FFFFFF", width=1)
-                    elif(not model.getPlayerBoard().getSpace(i,j).isAttacked() and model.getPlayerBoard().getSpace(i,j).isOccupied()):
-                        space = self._w.create_rectangle(x1, y1, x2, y2, fill="#AAA9AD", width=1)
-                    else:
-                       space = self._w.create_rectangle(x1, y1, x2, y2, fill="#00FFFF", width=1)
-
-                    if(model.getPlayerBoard().getSpace(i,j).isSelected()):
-                        self._w.itemconfig(space, outline="red", width=5)
-                    
-                    y1+=yDiff
-                    y2+=yDiff
-                x1+=xDiff
-                x2+=xDiff
+                y1+=yDiff
+                y2+=yDiff
+            x1+=xDiff
+            x2+=xDiff
 
 
